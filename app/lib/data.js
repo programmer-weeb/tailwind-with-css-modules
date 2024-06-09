bcd04876891cb62b82a426d05ae8737de3cac5c9
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2024-06-09 15:25:32 +0300

    fetchStudents and change pagination to 10

--- app/lib/data.js
+++ app/lib/data.js
@@ -4,12 +4,12 @@ import { connectToDB } from "./utils";
 export const fetchUsers = async (q, page) => {
   const regex = new RegExp(q, "i");
 
-  const ITEM_PER_PAGE = 2;
+  const ITEM_PER_PAGE = 10;
 
   try {
     connectToDB();
-    const count = await User.find({ username: { $regex: regex } }).count();
-    const users = await User.find({ username: { $regex: regex } })
+    const count = await StudentModelSchema.find({ username: { $regex: regex } }).count();
+    const users = await StudentModelSchema.find({ username: { $regex: regex } })
       .limit(ITEM_PER_PAGE)
       .skip(ITEM_PER_PAGE * (page - 1));
     return { count, users };
@@ -20,25 +20,30 @@ export const fetchUsers = async (q, page) => {
 };
 
 export const fetchStudents = async (q, page) => {
+
   const regex = new RegExp(q, "i");
 
-  const ITEM_PER_PAGE = 2;
+  const ITEM_PER_PAGE = 10;
 
   try {
     connectToDB();
     const count = await StudentModelSchema.find({ username: { $regex: regex } }).count();
-    
-   } catch (err) {
+    const users = await StudentModelSchema.find({ username: { $regex: regex } })
+      .limit(ITEM_PER_PAGE)
+      .skip(ITEM_PER_PAGE * (page - 1));
+    return { count, users };
+  } catch (err) {
     console.log(err);
-    throw new Error("Failed to fetch students!");
+    throw new Error("Failed to fetch users!");
   }
+
 }
 
 export const fetchUser = async (id) => {
   console.log(id);
   try {
     connectToDB();
-    const user = await User.findById(id);
+    const user = await StudentModelSchema.findById(id);
     return user;
   } catch (err) {
     console.log(err);
@@ -50,7 +55,7 @@ export const fetchProducts = async (q, page) => {
   console.log(q);
   const regex = new RegExp(q, "i");
 
-  const ITEM_PER_PAGE = 2;
+  const ITEM_PER_PAGE = 10;
 
   try {
     connectToDB();
