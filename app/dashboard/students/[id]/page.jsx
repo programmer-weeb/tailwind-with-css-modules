c2b7c966ad0c875f43a23a9915db0174bd695d88
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2024-06-12 12:14:16 +0300

    Remove a waring

--- app/dashboard/students/[id]/page.jsx
+++ app/dashboard/students/[id]/page.jsx
@@ -2,8 +2,9 @@ import {fetchStudent} from "../../../lib/data.js";
 import StudentDetails from "./StudentDetails";
 
 const StudentDetailsServer = async ({ params }) => {
-	const currentStudent = await fetchStudent(params.id);
-	// console.log(currentStudent);
+	const currentStudentWithAllProps = await fetchStudent(params.id);
+	const {username, email, medicalRecords, parentName, parentEmail, parentPhone} = currentStudentWithAllProps
+	const currentStudent = {username, email, medicalRecords, parentName, parentEmail, parentPhone}
 	console.log(params);
 	console.log(currentStudent);
 	return (
