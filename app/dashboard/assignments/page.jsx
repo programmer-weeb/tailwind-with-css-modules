2ad476264c1113b945d94299e3615c072bf62503
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2024-06-13 10:48:24 +0300

    Better teacher dashboard

--- app/dashboard/assignments/page.jsx
+++ app/dashboard/assignments/page.jsx
@@ -1,7 +1,7 @@
 import AssignmentsPage from "../../ui/student-details-stuff/AssignmentsPage.jsx";
 import {auth} from "../../auth.js";
 
-export default async function Page() {
+export default async function AssignmentsPageServer() {
   const { user } = await auth();
   const role = user.role
   return(
--- app/ui/myDashboard/TeacherDashboard.jsx
+++ app/ui/myDashboard/TeacherDashboard.jsx
@@ -1,5 +1,21 @@
-export default function TeacherDashboard() {
+import StudentsPage from "../../dashboard/students/page.jsx";
+import ExamsPage from "../../dashboard/exams/page.jsx";
+import AssignmentsPageServer from "../../dashboard/assignments/page.jsx";
+
+export default async function TeacherDashboard() {
 	return (
-		<h1>Teacher Dashboard</h1>
+		<div>
+			<div className="flex">
+				<div className="w-1/2">
+					<ExamsPage/>
+				</div>
+				<div className="w-1/2">
+					<AssignmentsPageServer/>
+				</div>
+			</div>
+			<div className="mt-4">
+				<StudentsPage/>
+			</div>
+		</div>
 	)
 }
