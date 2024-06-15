1f7f065d1a007712c37a4b1effc4ae79d72c1a27
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2024-06-15 14:33:34 +0300

    Change tab title

--- app/layout.js
+++ app/layout.js
@@ -5,8 +5,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Lama Dev Next.js Admin Dashboard',
-  description: 'Next.js 14 Tutorial',
+  title: 'Atlas MS',
+  description: 'Next.js 14 Project',
 }
 
 export default function RootLayout({ children }) {
