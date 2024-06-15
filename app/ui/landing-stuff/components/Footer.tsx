918f0ed16ab438e8e86353b5168b97c66cb731de
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2024-06-15 18:28:58 +0300

    Put github link in landing page

--- app/ui/landing-stuff/components/Footer.tsx
+++ app/ui/landing-stuff/components/Footer.tsx
@@ -22,7 +22,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" mt={1}>
       {'Copyright © '}
-      <Link href="https://mui.com/">Sitemark&nbsp;</Link>
+      <Link href="https://github.com/programmer-weeb/tailwind-with-css-modules">Atlas&nbsp;</Link>
       {new Date().getFullYear()}
     </Typography>
   );
@@ -58,13 +58,7 @@ export default function Footer() {
         >
           <Box sx={{ width: { xs: '100%', sm: '60%' } }}>
             <Box sx={{ ml: '-15px' }}>
-              <img
-                src={
-                  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e6faf73568658154dae_SitemarkDefault.svg'
-                }
-                style={logoStyle}
-                alt="logo of sitemark"
-              />
+              
             </Box>
             <Typography variant="body2" fontWeight={600} gutterBottom>
               Newsletter
@@ -192,7 +186,7 @@ export default function Footer() {
         >
           <IconButton
             color="inherit"
-            href="https://github.com/mui"
+            href="https://github.com/programmer-weeb/tailwind-with-css-modules"
             aria-label="GitHub"
             sx={{ alignSelf: 'center' }}
           >
